512041c5296e8205d4001fab593c9834c84e8eb6
kobrak1 <burakkarhan657@gmail.com>
2023-09-12 14:41:04 +0300

    1.2: course information, step2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,26 +1,34 @@
+import React from 'react';
+
 const Header = (props) => {
   return (
     <h1>{props.course}</h1>
   )
 }
 
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  )
+}
+
 const Content = (props) => {
   return (
     <>
-      <p>{props.part1} {props.exercises1}</p>
-      <p>{props.part2} {props.exercises2}</p>
-      <p>{props.part3} {props.exercises3}</p>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
     </>
   )
 }
 
 const Total = (props) => {
   return (
-    <p>Number of excercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
   )
 }
-
-
 
 const App = () => {
   const course = 'Half Stack application development'
@@ -35,20 +43,20 @@ const App = () => {
     <div>
       <Header course={course}/>
       <Content 
-              part1={part1}
-              exercises1={exercises1}
-              part2={part2}
-              exercises2={exercises2}
-              part3={part3}
-              exercises3={exercises3}
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
       />
       <Total 
-              exercises1={exercises1}
-              exercises2={exercises2}
-              exercises3={exercises3}
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
       />
     </div>
   )
 }
 
-export default App  
+export default App;
