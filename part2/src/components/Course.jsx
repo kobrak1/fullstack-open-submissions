e7959b1d8c83fded570d9bd3582799e7021a59cf
kobrak1 <burakkarhan657@gmail.com>
2023-09-19 19:23:31 +0300

    2.5: separate module

--- part2/src/components/Course.jsx
+++ part2/src/components/Course.jsx
@@ -1,15 +1,15 @@
 const Course = ({header, course, total, courseIndex}) => {
     return (
         <>
-        <h2>{header}</h2>
-        <ul>
-            {course[courseIndex].parts.map(note => 
-            <li key={note.id}>
-                {note.name} {note.exercises}
-            </li>
-            )}
-        </ul>
-        <b>total of {total} exercises</b><br />
+            <h2>{header}</h2>
+            <ul>
+                {course[courseIndex].parts.map(note => 
+                <li key={note.id}>
+                    {note.name} {note.exercises}
+                </li>
+                )}
+            </ul>
+            <b>total of {total} exercises</b><br />
         </>
     )
 }
