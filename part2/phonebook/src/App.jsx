84fa2f9727fe5cb15a0b62e402bef3f357f3358f
kobrak1 <burakkarhan657@gmail.com>
2023-09-22 17:31:54 +0300

    2.8: The Phonebook Step3

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,24 +4,27 @@ import { useState } from 'react'
 const Numbers = ({persons}) => {
   return (
     <ul style={{ listStyleType: 'none' }}>
-      {persons.map(e => <li key={e.name}> {e.name} </li> )}
+      {persons.map(e => <li key={e.name}> {e.name}  {e.number} </li> )}
     </ul>
   )
 }
 
 
 const App = () => {
-  const [persons, setPersons] = useState([{name:'Arto Hellas'}])
+  const [persons, setPersons] = useState([{name:'Arto Hellas', number:'905303888230'}])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
   // handle function
   const addUser = (event) => {
-    event.preventDefault();
+    event.preventDefault(); 
     // condition
     if (persons.some(person => person.name === newName)) {
       return alert(`There is already a ${newName} in the list!`)
     } else {
-      setPersons([...persons, {name: newName}])
+      setPersons([...persons, {name: newName, number: newNumber}])
       setNewName('');
+      setNewNumber('');
     }
   }
 
@@ -35,6 +38,12 @@ const App = () => {
             value={newName}
             onChange={(e) => setNewName(e.target.value)}  
           />
+        </div>
+        <div>
+          number: <input 
+            type="number"
+            value={newNumber}
+            onChange={(e) => setNewNumber(e.target.value)} />
         </div>
         <div>
           <button type='submit'>add</button>
