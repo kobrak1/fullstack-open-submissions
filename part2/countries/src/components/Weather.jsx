6f27df8f8904cdd3b1282b6ef3f2d6ea514db786
kobrak1 <burakkarhan657@gmail.com>
2023-09-28 00:55:45 +0300

    2.20*: Data for countries, step3

---
+++ part2/countries/src/components/Weather.jsx
@@ -0,0 +1,21 @@
+
+const Weather = ({ city, weather }) => {
+
+    if (weather) {    
+    const getWeatherIcon = `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`
+    const windSpeed = (Math.round((weather.wind.speed * 3.6) * 100) / 100).toFixed(2)
+    const compassSector = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW", "N"];
+    const windDirection = compassSector[(weather.wind.deg / 22.5).toFixed(0)];
+    return (
+    <div>
+        <h2>Weather in {city}</h2>
+        <p><strong>Temperature: </strong> {weather.main.temp}° Celsius</p>
+        <img src={getWeatherIcon} alt='weatherIcon'/>
+        <p><strong>Wind: </strong>{windSpeed} km/h direction {windDirection}</p>
+    </div>
+  )}
+  return <h2>No weather report available for this country</h2>
+}
+
+
+export default Weather
