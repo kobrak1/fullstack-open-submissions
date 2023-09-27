aeacb08724cb39edc5d2aeb446d68f7fd1dd980b
kobrak1 <burakkarhan657@gmail.com>
2023-09-27 23:50:06 +0300

    2.19*: Data for countries, step2

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,58 +1,87 @@
-import { useEffect, useState } from "react"
-import axios  from "axios"
-import './App.css'
+import { useEffect, useState } from "react";
+import axios from "axios";
+import './App.css';
 
-//components
-const Display = ({inputValue, setInputValue, filteredCountries}) => {
+// CountryDetail component to display detailed information for a single country
+const CountryDetail = ({ country, onClose }) => {
+  return (
+    <div>
+      <h2>{country.name.common} Details</h2>
+      <p>Region: {country.region}</p>
+      <p>Capital: {country.capital}</p>
+      <p>Area: {country.area}</p>
+      <img src={country.flags.png} alt="#" /> <br />
+      <button onClick={onClose}>Close</button>
+    </div>
+  );
+}
+
+// Display component with the list of countries and buttons to show/hide details
+const Display = ({ inputValue, setInputValue, filteredCountries }) => {
+  const [selectedCountry, setSelectedCountry] = useState(null);
+
+  const showDetails = (country) => {
+    setSelectedCountry(country);
+  };
+
+  const hideDetails = () => {
+    setSelectedCountry(null);
+  };
+
   return (
     <>
       <input 
-      type="text"
-      value={inputValue}
-      onChange={e => setInputValue(e.target.value)}
-      placeholder="Search..." />
+        type="text"
+        value={inputValue}
+        onChange={e => setInputValue(e.target.value)}
+        placeholder="Search..." />
       <ul>
         {filteredCountries.map((country, index) => (
           <li key={index}>
-            {country.name.common} <br />
-            Capital: {country.capital} <br />
-            Area: {country.area} <br />
-            {/* Language: {country.languages} <br /> */}
-            <img src={country.flags.png} alt="#" /> </li>
+            {country.name.common}{' '}
+            {selectedCountry === country ? (
+              <button onClick={hideDetails}>Hide Details</button>
+            ) : (
+              <button onClick={() => showDetails(country)}>Show Details</button>
+            )}
+            {selectedCountry === country && (
+              <CountryDetail country={country} onClose={hideDetails} />
+            )}
+          </li>
         ))}
       </ul>
     </>
-  )
+  );
 }
 
-
 const App = () => {
-  const [inputValue, setInputValue] = useState('')
-  const [countries, setCountries] = useState([])
-  const [filteredCountries, setFilteredCountries] = useState([])
+  const [inputValue, setInputValue] = useState('');
+  const [countries, setCountries] = useState([]);
+  const [filteredCountries, setFilteredCountries] = useState([]);
 
   useEffect(() => {
-    //get data from API
+    // Get data from API
     axios
       .get('https://restcountries.com/v3.1/all')
       .then((response) => setCountries(response.data))
-      .catch(error => {console.error('There is an issue while getting data from server:', error)
-    });
-  }, [])
+      .catch(error => {
+        console.error('There is an issue while getting data from server:', error);
+      });
+  }, []);
 
   useEffect(() => {
-    //filter countries that matches our input value
+    // Filter countries that match our input value
     const filtered = countries.filter(e => 
-      e.name.common.toLowerCase().includes(inputValue.toLocaleLowerCase()))
+      e.name.common.toLowerCase().includes(inputValue.toLocaleLowerCase()));
     if (inputValue === '') {
-      setFilteredCountries([])
+      setFilteredCountries([]);
     } else if (filtered.length > 0) {
-      setFilteredCountries(filtered)
+      setFilteredCountries(filtered);
     } else {
-      // if we don't have any match return an empty array
-      setFilteredCountries([])
+      // If we don't have any match, return an empty array
+      setFilteredCountries([]);
     }
-  }, [inputValue, countries])
+  }, [inputValue, countries]);
 
   return (
     <div>
@@ -62,7 +91,7 @@ const App = () => {
         setInputValue={setInputValue}
         filteredCountries={filteredCountries} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
