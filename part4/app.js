0fb085fdfb9e9acd08cdeb3c0bedb965e240c05d
kobrak1 <burakkarhan657@gmail.com>
2024-03-10 11:54:33 +0300

    4.2 Blog List, step 2

--- part4/app.js
+++ part4/app.js
@@ -1,27 +1,32 @@
-require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
-//const blogsRouter = require('./controllers/blogs')
-const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
-const morgan = require('morgan')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
 
 mongoose.set('strictQuery', false)
 
-//connect to mongoDB
-mongoose.connect(process.env.MONGODB_URI)
-    .then(() => {
-        console.log('connected to mongoDB')
-    })
-    .catch(error => {
-        console.log('error connecting to mongoDB:', error.message)
-    })
+logger.info(`Connecting to ${config.MONGODB_URI}`)
+
+// connect to mongoDB
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    console.log('connected to mongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to mongoDB:', error.message)
+  })
 
 app.use(cors())
 app.use(express.json())
-app.use(morgan('dev'))
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndPoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -3,32 +3,30 @@ const blogsRouter = require('express').Router()
 
 // check if the server works
 blogsRouter.get('/', (request, response) => {
-    if (response.statusCode !== 200) {
-        console.log('response is not ok')
-    }
-    console.log('response is ok')
-    response.send('hello mars hellooo')
+  if (response.statusCode !== 200) {
+    console.log('response is not ok')
+  }
+  console.log('response is ok')
+  response.send('hello mars hellooo')
 })
 
 // get all blogs
 blogsRouter.get('/all', (request, response) => {
-    Blog.find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
+  Blog.find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
 })
 
 // post a blog
-blogsRouter.post('/', (request, response) => {
-    const blog = new Blog(request.body)
+blogsRouter.post('/', (request, response, next) => {
+  const blog = new Blog(request.body)
 
-    blog.save()
-        .then(savedBlog => {
-            response.json(savedBlog)
-        })
-        .catch(error => {
-            console.log('Error:', error.message)
-        })
+  blog.save()
+    .then(savedBlog => {
+      response.json(savedBlog)
+    })
+    .catch(error => next(error))
 })
 
 module.exports = blogsRouter
