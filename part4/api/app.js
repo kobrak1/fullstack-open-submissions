bf83d676e1e38c93d06f24b3d239b2855d8b978c
kobrak1 <burakkarhan657@gmail.com>
2024-05-07 16:38:07 +0300

    chore: moved to api folder

---
+++ part4/api/app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+
+mongoose.set('strictQuery', false)
+
+logger.info(`Connecting to ${config.MONGODB_URI}`)
+
+// connect to mongoDB
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    console.log('connected to mongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to mongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+app.use(middleware.tokenExtractor)
+
+app.use('/api/blogs', blogsRouter) // to attach '/api/blogs' to blogsRouter
+app.use('/api/users', usersRouter) // to attach '/api/users' to usersRouter
+app.use('/api/login', loginRouter) // to attach '/api/login' to loginRouter
+
+app.use(middleware.unknownEndPoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
